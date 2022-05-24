5a5897efe3e824639226382921a3d4289a47009a
Moonmoo <ansaudwn1234@gmail.com>
2022-05-24 22:30:57 +0900

    좋아요 및 count 기능 추가

--- article_detail.js
+++ article_detail.js
@@ -1,7 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const article_id = urlParams.get('id');
 console.log(article_id)
-
+let liked = false
 
 
 async function loadArticle(article_id) {
@@ -12,11 +12,12 @@ async function loadArticle(article_id) {
     const content = document.getElementById('content')
     const user_email = document.getElementById('user_email')
     const time = document.getElementById('time')
+    const like_button = document.getElementById('like_button')
     title.innerText = article.title
     content.innerText = article.content
     user_email.innerText = article.user_email
     time.innerText = article.time
-    console.log(article.comments)
+    like_button.innerText = article.likes_count
 
     const comment_section = document.getElementById('comment_section')
     comment_section.innerHTML = ''
@@ -27,6 +28,7 @@ async function loadArticle(article_id) {
         comment_section.appendChild(new_comment)
     }
 
+    uppdateLike()
     const user = await getName()
     if (user.id != article.user) {
         const update_button = document.getElementById('update_button')
@@ -45,6 +47,7 @@ function updateMode() {
     const input_title = document.createElement('textarea')
     input_title.setAttribute('id', 'input_title')
     input_title.innerText = title.innerHTML
+
 
     const input_content = document.createElement('textarea')
     input_content.setAttribute('id', 'input_content')
@@ -91,5 +94,32 @@ async function writeComment() {
 
 }
 
+async function likeArticle() {
+    const like_button = document.getElementById('like_button')
+    like_button.classList.toggle('fa-thumbs-down')
+
+    if (!liked) {
+        const response = await postLike(article_id)
+        console.log(response, '좋아요')
+        like_button.innerText = parseInt(like_button.innerText) + 1
+        liked = true
+    } else {
+        const response = await deleteLike(article_id)
+        console.log(response, '취소')
+        like_button.innerText = parseInt(like_button.innerText) - 1
+        liked = false
+    }
+}
+
+async function uppdateLike() {
+    const response = await getLike(article_id)
+    console.log(response)
+    liked = response.liked
+    if (liked) {
+        like_button.classList.toggle('fa-thumbs-down')
+    }
+
+
+}
 
 loadArticle(article_id)
